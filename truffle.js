0bb49a10638b5d4654f216614bcc40ee0a22b843
Peter McQuillan <3148265+IronHED@users.noreply.github.com>
2018-03-01 21:32:11 -0500

    Added Contracts

--- truffle.js
+++ truffle.js
@@ -1,4 +1,19 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    live: {
+      host: 'localhost', // Random IP for example purposes (do not use)
+      port: 8545,
+      network_id: 1,        // Ethereum public network
+      from: '0x000fe3B327e85859eb26e9C6237fa6a34880CD68',
+      gas: 1000000
+    }
+  }
 };
